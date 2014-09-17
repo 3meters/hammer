f74cea656a0ec8d7d5ed49577c08df85c6a60fb7
George Snelling <georgesn@gmail.com>
2014-09-16 18:03:25 -0700

    basic command line parsing

--- hammer.go
+++ hammer.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"os"
+	"flag"
 	"fmt"
-	//  "io"
-	//  "log"
-	//  "net/http"
 )
 
-func main() {
-	fmt.println("hello world/n")
+var configFileName string
+var helpMe bool 
+
+func init() {
+  flag.StringVar(&configFileName, "config", "config.json", "config file")
+	flag.StringVar(&configFileName, "c", "config.json", "config file")
+	flag.BoolVar(&helpMe, "help", false, "help")
+	flag.BoolVar(&helpMe, "h", false, "help")
 }
 
+
+func main() {
+
+	flag.Parse()
+
+	if helpMe {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	fmt.Println("hello " + configFileName)
+}
