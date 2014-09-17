c833bcb00cf8d8a3c569a82833f90117fb3baeed
George Snelling <georgesn@gmail.com>
2014-09-17 16:03:50 -0700

    hits the root url and pretty-prints the response

--- hammer.go
+++ hammer.go
@@ -3,34 +3,38 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
+	"net/http"
+	"os"
 )
 
 var configFileName string
-var helpMe bool 
+var helpMe bool
 
 type Config struct {
-	Host			string
-	UserName	string
-	Password	string
-	UserId		string
-	Session		string
-	Hammers		int
-	Seconds		int
+	Host     string
+	UserName string
+	Password string
+	UserId   string
+	Session  string
+	Cred     string
+	Hammers  int
+	Seconds  int
+	paths    []string
 }
 
 // Set command line flags
 func init() {
-  flag.StringVar(&configFileName, "config", "config.json", "config file")
+	flag.StringVar(&configFileName, "config", "config.json", "config file")
 	flag.StringVar(&configFileName, "c", "config.json", "config file")
 	flag.BoolVar(&helpMe, "help", false, "help")
 	flag.BoolVar(&helpMe, "h", false, "help")
 }
-
 
 func main() {
 
@@ -45,16 +49,39 @@ func main() {
 
 	content, err := ioutil.ReadFile(configFileName)
 	if err != nil {
-		fmt.Print("Could not read config file " + configFileName, err)
+		fmt.Print("Could not read config file "+configFileName, err)
 		os.Exit(1)
 	}
 
 	var conf Config
 	err = json.Unmarshal(content, &conf)
 	if err != nil {
-		fmt.Print("Error parsing config file: ", err)
+		fmt.Println("Error parsing config file:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(conf)
+	fmt.Println("Config: ", conf)
+
+	// Configure a transport that accepts self-singed certs
+	// Same as curl --insecure
+	tr := &http.Transport{
+		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},  // accept self-signed certificates
+		DisableCompression: true,
+	}
+	client := &http.Client{Transport: tr}
+
+	// Run get on the host url to 
+	res, err := client.Get(conf.Host)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	var indented bytes.Buffer
+	json.Indent(&indented, body, "", "  ")
+	fmt.Printf("%s", indented)
+	fmt.Println()
+
 }
