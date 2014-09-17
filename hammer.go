a8e0b5ee42b06bee91219eb5ff7793604208f60b
George Snelling <georgesn@gmail.com>
2014-09-17 14:39:30 -0700

    read a json config file

--- hammer.go
+++ hammer.go
@@ -1,14 +1,27 @@
+// Http client hammer for stress-testing web services
+
 package main
 
 import (
 	"os"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"encoding/json"
 )
 
 var configFileName string
 var helpMe bool 
 
+type Config struct {
+	Host			string
+	UserName	string
+	Password	string
+	UserId		string
+	Session		string
+	Hammers		int
+	Seconds		int
+}
 
 // Set command line flags
 func init() {
@@ -28,5 +41,20 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println("hello " + configFileName)
+	fmt.Println("Reading config file " + configFileName)
+
+	content, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		fmt.Print("Could not read config file " + configFileName, err)
+		os.Exit(1)
+	}
+
+	var conf Config
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
+		fmt.Print("Error parsing config file: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(conf)
 }
